e198e33c9718720b3902ae2f3ee4963bd5bb0754
KoenVA <koenvanaken1999@gmail.com>
2024-10-20 16:45:13 +0200

    Add header file

---
+++ src/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H // prevent multiple inclusions of header file
+#define MAIN_H
+
+// function prototypes
+int addition();
+int subtraction();
+int multiplication();
+int division();
+
+#endif // MAIN_H
